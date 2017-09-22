125cd820e044b0b1bd8c8b5afa13aa2888eccef6
TaeWoo Kim <kitawo324@gmail.com>
2017-09-22 13:27:25 +0900

    string tset

--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,18 @@
 #include "utils.h"
-
+#include <string.h>
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
   // TODO: Fill this!
+  // char *argv = String
+  // char **argv = StringPtr = (**argv)[0]
+  // char ***argv = StringPtrArray
+  **argv  = strtok(command, " ");
+  *argc = 1;
+  while( **argv = strtok(NULL, " "))
+  {
+     *argc +=1;
+  }
+  return 0;
+  
 }
